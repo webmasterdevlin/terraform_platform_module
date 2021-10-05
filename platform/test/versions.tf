40553b613046d8062b122889c6acade4ca941cf9
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-06 00:14:14 +0200

    update

--- platform/test/versions.tf
+++ platform/test/versions.tf
@@ -5,6 +5,30 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 2.79.1"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.5.0"
+    }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "~> 1.2.6"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.1.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.7.2"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.1.0"
+    }
   }
 
   backend "azurerm" {}
