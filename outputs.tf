ed9ec8fa23eb35fb05c431a2874e4b1ce7e7de13
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-06 22:50:56 +0200

    update

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 ## demo code, remove me
 output "platform_rg_name" {
-  value = module.name_of_platform
+  value = module.name_of_platform.platform_rg_name
 }
 
 ## demo code, remove me
-output "platform_location" {
-  value     = module.name_of_platform
+output "platform_rg_location" {
+  value     = module.name_of_platform.platform_rg_location
   sensitive = true
 }
--- platform/production/outputs.tf
+++ platform/production/outputs.tf
@@ -4,6 +4,6 @@ output "platform_rg_name" {
 }
 
 ## demo code, remove me
-output "platform_location" {
+output "platform_rg_location" {
   value = azurerm_resource_group.platform.location
 }
