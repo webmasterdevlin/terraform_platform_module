dea18522500533567187260f83deb8d98372cde7
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-06 22:53:08 +0200

    update

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 ## demo code, remove me
-output "platform_rg_name" {
+output "rg_name" {
   value = module.name_of_platform.platform_rg_name
 }
 
 ## demo code, remove me
-output "platform_rg_location" {
+output "rg_location" {
   value     = module.name_of_platform.platform_rg_location
   sensitive = true
 }
