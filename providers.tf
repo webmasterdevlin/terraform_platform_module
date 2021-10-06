98a9a8ec615968c54803aac6eed6a5846872f949
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-06 22:08:44 +0200

    update

--- providers.tf
+++ providers.tf
@@ -2,8 +2,8 @@ provider "azurerm" {
   features {}
 
   ## Azure credentials for logging to Terraform Cloud
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
 }
--- variables.tf
+++ variables.tf
@@ -8,19 +8,19 @@ variable "platform_rg_location" {
 
 
 ###### for initialization
-variable "subscription_id" {
-  description = "This is the subscription id. Then TF_VAR_subscription_id in environment variables of Terraform Workspace."
-  type        = string
-}
-variable "client_id" {
-  description = "This is the appId. Then TF_VAR_client_id in environment variables of Terraform Workspace."
-  type        = string
-}
-variable "client_secret" {
-  description = "This is the password. Then TF_VAR_client_secret in environment variables of Terraform Workspace."
-  type        = string
-}
-variable "tenant_id" {
-  description = "This is the tenant. Then TF_VAR_tenant_id in environment variables of Terraform Workspace."
-  type        = string
-}
+# variable "subscription_id" {
+#   description = "This is the subscription id. Then TF_VAR_subscription_id in environment variables of Terraform Workspace."
+#   type        = string
+# }
+# variable "client_id" {
+#   description = "This is the appId. Then TF_VAR_client_id in environment variables of Terraform Workspace."
+#   type        = string
+# }
+# variable "client_secret" {
+#   description = "This is the password. Then TF_VAR_client_secret in environment variables of Terraform Workspace."
+#   type        = string
+# }
+# variable "tenant_id" {
+#   description = "This is the tenant. Then TF_VAR_tenant_id in environment variables of Terraform Workspace."
+#   type        = string
+# }
