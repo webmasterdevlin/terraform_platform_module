3ce135e3c0833e0d5522a8752ea9368a6bb4b4b9
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-06 22:38:06 +0200

    update

--- main.tf
+++ main.tf
@@ -3,3 +3,14 @@ module "name_of_platform" {
   platform_rg_name     = var.platform_rg_name
   platform_rg_location = var.platform_rg_location
 }
+
+
+output "platform_rg_name" {
+  value = module.name_of_platform.platform_rg_name
+}
+
+## demo code, remove me
+output "platform_location" {
+  value     = module.name_of_platform.platform_rg_location
+  sensitive = true
+}
--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,2 @@
 ## demo code, remove me
-output "platform_rg_name" {
-  value = module.name_of_platform.name
-}
 
-## demo code, remove me
-output "platform_location" {
-  value     = module.name_of_platform.location
-  sensitive = true
-}
